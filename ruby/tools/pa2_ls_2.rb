be46c94662220a283494f239dd38beaf93b91140
Lane Mills <mrlaneiscool@gmail.com>
2017-04-28 22:39:03 -0500

    workshop - Added option for '-R' command

--- ruby/tools/pa2_ls_2.rb
+++ ruby/tools/pa2_ls_2.rb
@@ -11,7 +11,7 @@ def directory_hash(path, name=nil)
 	data[:children] = children = []
 	puts path+"/"
 	Dir.foreach(path) do |entry|
-		next if(entry == '..'|| entry =='.')
+		next if(entry == '..'|| entry =='.' || entry =='.git')
 		full_path = File.join(path, entry)
 		if File.directory?(full_path)
 			children << directory_hash(full_path,entry)
@@ -23,9 +23,16 @@ def directory_hash(path, name=nil)
 	return data
 end
 
+def print_dir(path)
+	puts Dir[path+"/*"]
+end
+
 # ** Main code **
 
 # Change to home directory (ruby likes to start from where it's run)
+files = 0
+dirs = 0
+total = 0
 Dir.chdir("/home/oscreader/")
 # Welcome/instructions
 puts "Enter a file or directory, or leave blank"
@@ -34,6 +41,10 @@ puts "Starting from '"+Dir.pwd+"/'"
 # Take user input
 start = gets
 # Run method 'walk' with the user's input
-data_hash = directory_hash(Dir.pwd+"/"+start.gsub(/\n/,""))
+if start.include?("-R")
+	directory_hash(Dir.pwd+"/"+start.gsub(/(-R)|\s+|\n/,""))
+else 
+	print_dir(Dir.pwd+"/"+start.gsub(/(-R)|\s+|\n/,""))
+end
 puts "*****************"
 
