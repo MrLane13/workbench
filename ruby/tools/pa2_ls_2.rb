6d495a44372e51193139518f8d3bed56b39ac2d7
Lane Mills <mrlaneiscool@gmail.com>
2017-04-30 20:53:56 -0500

    workshop - Added counting to recursive file traverse

--- ruby/tools/pa2_ls_2.rb
+++ ruby/tools/pa2_ls_2.rb
@@ -6,7 +6,11 @@ require 'find'
 # Date - 01 May 2017
 
 # ** Methods **
-def directory_hash(path, name=nil)
+
+# Recursive travers, build hash.
+# A hash seemed like the nicest way to get the desired output format.
+def directory_hash(apath, name=nil)
+	path = apath.gsub(/(-R)|\s+|\n/,"")
 	data = {:data => [name || path]}
 	data[:children] = children = []
 	puts path+"/"
@@ -20,31 +24,66 @@ def directory_hash(path, name=nil)
 			puts entry.gsub(/\n|\r|/,"")
 		end
 	end
-	return data
+	rescue 
+		puts "ERROR : could not traverse path: "+path
 end
 
-def print_dir(path)
+# Traverse current directory
+def print_dir(apath)
+	path = apath.gsub(/(-R)|\s+|\n/,"")
 	puts Dir[path+"/*"]
+	return Dir[path+"/*"]
+end
+# Count files recursively
+def loop_count_file(apath)
+	path = apath.gsub(/(-R)|\s+|\n/,"")
+	return Dir.glob(File.join(path,'**','*'))
+					.select {|file| File.file?(file)}.count
+end
+# Count directories recursively
+def loop_count_dirs(apath)
+	path = apath.gsub(/(-R)|\s+|\n/,"")
+	return Dir.glob(File.join(path,'**','*'))
+					.select {|dir| File.directory?(dir)}.count
 end
 
 # ** Main code **
 
 # Change to home directory (ruby likes to start from where it's run)
-files = 0
-dirs = 0
-total = 0
+dir = Dir[]
+file_count = 0
+dir_count = 0
+total_count = 0
 Dir.chdir("/home/oscreader/")
 # Welcome/instructions
+puts "*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*"
+puts "*********************************************"
 puts "Enter a file or directory, or leave blank"
 puts "OPTIONAL: add '-R' for recursive walk"
 puts "Starting from '"+Dir.pwd+"/'"
 # Take user input
 start = gets
-# Run method 'walk' with the user's input
+puts "***********FILES AND DIRECTORIES**************"
+# Check user input for '-R'
 if start.include?("-R")
-	directory_hash(Dir.pwd+"/"+start.gsub(/(-R)|\s+|\n/,""))
+	directory_hash(Dir.pwd+"/"+start)
+	puts "Loading..."
+	file_count = loop_count_file(start)
+	dir_count = loop_count_dirs(start)
 else 
-	print_dir(Dir.pwd+"/"+start.gsub(/(-R)|\s+|\n/,""))
+	dir = print_dir(Dir.pwd+"/"+start)
+	dir.each {|val| if File.directory?(val) 
+				then 
+					dir_count=dir_count+1
+				else 
+					file_count=file_count+1 
+				end}
 end
-puts "*****************"
-
+total_count = file_count+dir_count
+puts "*****************SUMMARY*********************"
+# Summaries
+puts "Files: "+file_count.to_s
+puts "Dirs : "+dir_count.to_s
+puts "Total: "+total_count.to_s
+puts "*********************************************"
+puts "*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*"
